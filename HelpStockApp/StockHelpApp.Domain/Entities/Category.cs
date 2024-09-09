f73392cce18f3b01c2aa2b36cba381eed58899e7
joseAntonioFatec <jose_antonio_alves@protonmail.com>
2024-09-09 12:28:58 -0300

    Aula0909 - validação de entidades no dominio

--- HelpStockApp/StockHelpApp.Domain/Entities/Category.cs
+++ HelpStockApp/StockHelpApp.Domain/Entities/Category.cs
@@ -1,5 +1,7 @@
 ﻿using StockHelpApp.Domain.Entities;
+using StockHelpApp.Domain.Validation;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +15,22 @@ namespace StockHelpApp.Domain.Endities
 
         public Category(string name)
         { 
-            this.Name = name; 
+            validateDomain(name); 
         }
 
         public Category(int id,string name) 
         { 
             Id = id;
+            validateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+    
+        private void validateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),"Invalid name, name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short. minimum 3 characteres!");
+
             Name = name;
         }
     }
