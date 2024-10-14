691677295696e3da5f81332b7e84fbf770e98297
joseAntonioFatec <jose_antonio_alves@protonmail.com>
2024-10-14 12:32:49 -0300

    Aula1409 - Implementação category e product

---
+++ HelpStockApp/HelpStockApp.Infra.Data/EntityConfiguration/categoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using HelpStockApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HelpStockApp.Infra.Data.EntityConfiguration
+{
+    public class categoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+
+
+
+    }
+
+}
